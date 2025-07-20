de8d5166ff28982bc0c52ddf6f1dbef5dca2eb8f
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-07-20 22:10:17 +0300

    Initial commit
    Also co-authoring the authors of patches and base repo.
    
    Co-authored-by: Michal Kubecek <mkubecek@suse.cz>
    Co-authored-by: Julian Xhokaxhiu <info@julianxhokaxhiu.com>

---
+++ extract-and-patch-modules.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for i in vmmon vmnet; do
+  rm -rf $i-only
+  tar -xvf /usr/lib/vmware/modules/source/$i.tar
+done
+
+for i in vmmon vmnet linux6_15; do
+  patch -p1 < patches/$i.patch
+done
